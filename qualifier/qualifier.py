beea1b4f66a9f5be509958354e1756552d13d35e
slimeborger <109778583+slimeborger@users.noreply.github.com>
2023-08-22 21:14:03 +1000

    attempt 1

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -1,3 +1,5 @@
+from PIL import Image
+
 def valid_input(image_size: tuple[int, int], tile_size: tuple[int, int], ordering: list[int]) -> bool:
     """
     Return True if the given input allows the rearrangement of the image, False otherwise.
@@ -5,9 +7,41 @@ def valid_input(image_size: tuple[int, int], tile_size: tuple[int, int], orderin
     The tile size must divide each image dimension without remainders, and `ordering` must use each input tile exactly
     once.
     """
+    
+    width_divisible = image_size[0] % tile_size[0] == 0 
+    height_divisible = image_size[1] % tile_size[1] == 0
+
+    tile = tile_size[0]*tile_size[1]
+    image = image_size[0]*image_size[1]
+    if width_divisible and height_divisible:
+        tiles = {i for i in range(int(image/tile)) }
+        if set(ordering) == tiles:
+            return True
+    return False
+    
 
 
 def rearrange_tiles(image_path: str, tile_size: tuple[int, int], ordering: list[int], out_path: str) -> None:
+    image = Image.open(image_path)
+    if not valid_input(image.size,tile_size,ordering):
+        return False
+    
+    tiles = (image.size[0]/tile_size[0], image.size[1]/tile_size[1])
+
+    output_img = Image.new("RGBA",image.size)
+    sub_imgs = []
+    for i in ordering:
+        region = (i*tile_size[0],
+                  i*tile_size[1],
+                  tile_size[0]*(i+1),
+                  tile_size[1]*(i+1))
+        sub_imgs.append(image.crop(region))
+
+    positions = zip(range(tiles[0]),range(tiles[1]))    
+    for i in range(tiles[0]*tiles[1]):
+        output_img.paste(sub_imgs[i],positions[i])
+    return output_img
+
     """
     Rearrange the image.
 
@@ -18,3 +52,4 @@ def rearrange_tiles(image_path: str, tile_size: tuple[int, int], ordering: list[
     once. If these conditions do not hold, raise a ValueError with the message:
     "The tile size or ordering are not valid for the given image".
     """
+rearrange_tiles("/qualifier/images/pydis_logo_scrambled.png",(128,128),[0,3,1,2],"image.png")
